8108efb08d5235746f7af7ade3d753a23edbde62
sanechkae <cleeneya@gmail.com>
2020-12-19 03:15:49 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+ $p = 'Welcome to my page!';
+?>
+
+<?php 
+ $name = 'Aliaksandra';
+ $surname = 'Babock';
+ $city = 'St.Peterburg';
+ $age = 17;
+?>
+
+
+<?php
+include 'main.php';
+?>
---
+++ knowledge.inc.php
@@ -0,0 +1,12 @@
+<?php
+$a = 'Level of my knowledge:';
+$b = 100;
+$c = '%.'; 
+?>
+
+<?php
+  $price = 15;
+
+  if($price >= 15 && $price <= 20)
+      $d = 'This number is from 15 to 20.';
+?>
